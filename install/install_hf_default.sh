03b673b537aaa998b298dbe0e6244ec8c3da8987
Kevin Duh <kevinduh@cs.jhu.edu>
2024-11-30 19:19:05 -0500

    Add temporary install script

---
+++ install/install_hf_default.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+HF_COMMIT=19dabe96362803fb0a9ae7073d03533966598b17 # (huggingface transformers version 11/30/2024)
+
+# Get this version of HF Transformers
+rootdir="$(readlink -f "$(dirname "$0")/../")"
+cd $rootdir
+git submodule init
+git submodule update --recursive --remote transformers
+cd transformers
+git checkout $HF_COMMIT
+cd ..
+
+#$rootdir/install/install_hf_custom.sh -s $rootdir/transformers -e hfmt

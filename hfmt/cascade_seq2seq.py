3e656761fec3bc6ac58f5ee0d7b4fa6842cbb380
Nathaniel Robinson <n8rrobinson@gmail.com>
2025-03-19 00:38:55 -0400

    device map

--- hfmt/cascade_seq2seq.py
+++ hfmt/cascade_seq2seq.py
@@ -222,7 +222,8 @@ def run_flores_eval(checkpoint, flores_code, outs_file):
     model = AutoMod.from_pretrained(
         checkpoint,
         torch_dtype=torch_dtype,
-        pad_token_id=tokenizer.eos_token_id
+        pad_token_id=tokenizer.eos_token_id,
+		device_map="auto"
     ).to(device)
     # generation_config = GenerationConfig.from_pretrained(checkpoint)
     if not os.path.exists(outs_file):

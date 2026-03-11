fa321a83286f91f937bbf1c89bdc30ea90c2ae09
Kevin Duh <kevinduh@cs.jhu.edu>
2026-03-11 14:46:54 -0400

    modify settings for different grid

--- install/path.sh
+++ install/path.sh
@@ -2,5 +2,7 @@
 # It sets some system-specific paths and environments
 # Modify this to match your setup
 
-source /opt/anaconda3/etc/profile.d/conda.sh
+source /cluster/apps/anaconda3/2024.02/etc/profile.d/conda.sh
 conda activate hfmt
+module load cuda/toolkit/12.8.1-1 cudnn/9.1.1.17_cuda12 nccl/2.21.5-1_cuda12.4
+
